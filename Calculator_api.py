7b9410a32293ea5936736e78746b25d1cc95f334
maneesha345 <lutukurthimaneesha@gmail.com>
2025-06-26 00:46:58 +0530

    Initial commit with Calculator_api FastAPI code

---
+++ Calculator_api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Calculator API. Use /add, /subtract, /multiply, /divide"}
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": a + b}
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"result": a - b}
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"result": a * b}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    if b == 0:
+        return {"error": "Cannot divide by zero"}
+    return {"result": a / b}
